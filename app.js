ac17d3ed54094f3510d812104565bdf276920762
Johnson-Oshodi <johnsonoshodi2@gmai.com>
2024-02-08 08:45:55 -0700

    Local_modules

---
+++ app.js
@@ -0,0 +1,8 @@
+const mathLib = require ("./my_math_lib")
+const user = require ("./user")
+
+
+const sum = mathLib.add (20, 200)
+console.log(sum)
+
+user("Johnson Samson Oshodi")
---
+++ user.js
@@ -0,0 +1,5 @@
+function printsUsername (name) {
+    console.log(`Your name is  ${name}`)
+}
+
+module.exports = printsUsername
